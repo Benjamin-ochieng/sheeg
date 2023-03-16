825497f16cbe673e6d554402ee34cfa1e6ff85c4
Benjamin-ochieng <ochiengbenja@gmail.com>
2023-03-16 17:50:12 +0300

    feat: add authService

---
+++ src/services/authService.js
@@ -0,0 +1,25 @@
+import { createFetch } from '@vueuse/core';
+
+const baseUrl = 'https://invoices-api-6pka.onrender.com';
+const requestOptions = {
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+};
+
+const useAuthFetch = createFetch({
+  baseUrl,
+  combination: 'overwrite',
+  options: {
+    async beforeFetch({ options }) {
+      // eslint-disable-next-line no-param-reassign
+      options = {
+        ...requestOptions,
+      };
+      return { options };
+    },
+  },
+});
+
+export const authSignin = () => useAuthFetch('/signin', { immediate: false });
+export const authSignup = () => useAuthFetch('/signup', { immediate: false });
