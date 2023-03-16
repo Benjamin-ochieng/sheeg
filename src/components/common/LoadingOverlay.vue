bf38688298d81229f7c5abf450ab13f1671f40c9
Benjamin-ochieng <ochiengbenja@gmail.com>
2023-03-16 18:30:25 +0300

    chore: loading page overlay setup

---
+++ src/components/common/LoadingOverlay.vue
@@ -0,0 +1,9 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+// TODO:https://github.com/moyoujun/vue3-loading-overlay/blob/master/src/main/Component.vue
+</script>
+
+<style lang="scss" scoped></style>
