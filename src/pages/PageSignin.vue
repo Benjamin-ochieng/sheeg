6c1c037ee36d6498a59aee204a3f1c654a8b2ab3
Benjamin-ochieng <ochiengbenja@gmail.com>
2023-03-16 17:52:01 +0300

    feat: connect signing to backend

--- src/pages/PageSignin.vue
+++ src/pages/PageSignin.vue
@@ -8,13 +8,14 @@
       </p>
     </div>
     <div class="py-6 divide-y space-y-8">
-      <form action="" class="flex flex-col text-xs space-y-8">
+      <form @submit.prevent="signin" class="flex flex-col text-xs space-y-8">
         <base-input
-          label="Email address"
-          type="email"
-          placeholder="jane@mail.com"
+          label="User Name"
+          type="text"
+          placeholder="Jane Doe"
+          v-model="userName"
         />
-        <base-input label="password" type="password" />
+        <base-input label="password" type="password" v-model="password" />
         <p class="font-light tracking-tight">
           By contnuing, you accept our
           <a href="#" target="_blank"
@@ -31,7 +32,7 @@
             ></a
           >
         </p>
-        <base-button>Continue</base-button>
+        <base-button button-type="submit">Continue</base-button>
       </form>
       <div class="flex flex-col items-center justify-center space-y-4">
         <div class="-mt-3 bg-white px-2 text-xs font-light">or</div>
@@ -39,7 +40,7 @@
           <img :src="GoogleLogo" class="w-4 h-4 mr-2" />
           Continue with Google
         </base-button>
-        <base-button variant="secondary" class="w-full">
+        <base-button variant="secondary" action="signin" class="w-full">
           <img :src="AppleLogo" class="w-4 h-4 mr-2" />
           Continue with Apple
         </base-button>
@@ -60,11 +61,23 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import GoogleLogo from '../assets/images/google-logo.png';
 import AppleLogo from '../assets/images/apple-log.png';
 import BaseButton from '../components/common/BaseButton.vue';
 import Icon from '../components/common/Icon.vue';
 import BaseInput from '../components/common/BaseInput.vue';
+import { authSignin } from '../services/authService';
+
+const userName = ref('');
+const password = ref('');
+
+const { execute } = authSignin().post(() => ({
+  userName: userName.value,
+  password: password.value,
+}));
+
+const signin = () => execute();
 </script>
 
 <style lang="scss" scoped></style>
