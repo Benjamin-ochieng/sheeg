723120c4991910a958adb45271ed99a8e86be1ae
Benjamin-ochieng <ochiengbenja@gmail.com>
2023-03-23 09:01:37 +0000

    feat: add variant prop

--- src/components/common/BaseInput.vue
+++ src/components/common/BaseInput.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
-    <label :for="props.label" class="block mb-2">{{ label }}</label>
+    <label
+      :for="props.label"
+      class="block mb-2"
+      :class="{
+        'text-slate-900': variant === 'default',
+        'text-red-500': variant === 'desructive',
+        'text-slate-400': variant === 'ghost',
+      }"
+      >{{ label }}</label
+    >
     <input
-      class="block appearance-none w-full border border-slate-300 rounded-full py-3 px-4 leading-tight focus:outline-none focus:bg-white"
+      class="block appearance-none w-full border rounded-full py-3 px-4 leading-tight focus:outline-none"
+      :class="{
+        'border-red-500': variant === 'desructive',
+        'border-slate-400': variant === 'default',
+        'border-slate-300': variant === 'ghost',
+      }"
       :name="props.label"
       :id="props.label"
       :type="props.inputType"
@@ -22,14 +36,18 @@ const props = defineProps({
     type: String,
     default: 'Input',
   },
+  variant: {
+    type: String,
+    default: 'default',
+    validation: (value) => ['default', 'desructive', 'ghost'].includes(value),
+  },
   inputType: {
     type: String,
     default: 'text',
   },
-  // eslint-disable-next-line vue/require-default-prop
   placeholder: {
     type: String,
-    required: false,
+    default: '',
   },
   modelValue: {
     type: [Number, String],
@@ -38,4 +56,4 @@ const props = defineProps({
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="postcss" scoped></style>
